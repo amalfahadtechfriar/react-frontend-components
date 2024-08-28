90f8923816dccec3b8f9b6bc7d005b1608e38d17
Amal fahad <amalfahad@techfriar.com>
2024-08-28 09:48:14 +0530

    new-commit

---
+++ src/components/app-button/app-button.tsx
@@ -0,0 +1,50 @@
+import { Button } from "antd";
+import { MouseEventHandler } from "react";
+import { SizeType } from "antd/lib/config-provider/SizeContext";
+import styles from "./app-button.module.scss";
+
+export interface AppButtonProps {
+  size?: "small" | "middle" | "large" | "default" | undefined;
+  className?: string;
+  handleClick?: MouseEventHandler<HTMLElement>;
+  icon?: React.ReactNode;
+  text?: string;
+  disabled?: boolean;
+  iconPosition?: string;
+  isLoading?: boolean;
+  type?: "default" | "text" | "link" | "primary" | "dashed" | undefined;
+}
+
+const AppButton = ({
+  type,
+  size,
+  className,
+  handleClick,
+  icon,
+  text,
+  disabled,
+  iconPosition,
+  isLoading,
+}: AppButtonProps) => {
+  return (
+    <div className={[styles.appButton, className].join(" ")}>
+      <Button
+        disabled={disabled}
+        type={type}
+        className={[
+          styles.customBtn,
+          icon ? "btn-icon" : "",
+          iconPosition === "right" ? styles.iconRight : "",
+        ].join(" ")}
+        size={size as SizeType}
+        onClick={handleClick}
+        icon={icon}
+        loading={isLoading}
+      >
+        {text}
+      </Button>
+    </div>
+  );
+};
+
+export default AppButton;
