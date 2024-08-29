9a961020d372ba32df6545525fe46b2fb6ca9d1f
Amal fahad <amalfahad@techfriar.com>
2024-08-29 17:02:56 +0530

    new

--- index.ts
+++ index.ts
@@ -2,5 +2,5 @@ export * from "./src";
 
 export * from "./src/components/app-button/app-button";
 
-export { default } from "./src/components/app-button/app-button";
+export { default as AppButton } from "./src/components/app-button/app-button";
 export type { AppButtonProps } from "./src/components/app-button/app-button";
